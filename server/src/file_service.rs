363b86bad1d19b5af6a0a6e8c84cc0cfa83d914d
optimumood <optimumood@gmail.com>
2023-01-12 21:39:30 +0100

    Added tracing

--- server/src/file_service.rs
+++ server/src/file_service.rs
@@ -10,6 +10,7 @@ use tokio::fs;
 use tokio::sync::mpsc;
 use tokio_stream::wrappers::ReceiverStream;
 use tonic::{Request, Response, Status, Streaming};
+use tracing::{error, instrument, Instrument};
 
 #[derive(Default)]
 pub struct FileServiceImpl {
@@ -31,18 +32,23 @@ impl FileService for FileServiceImpl {
     type DownloadFileStream = ReceiverStream<Result<DownloadFileResponse, Status>>;
     type ListFilesStream = ReceiverStream<Result<ListFilesResponse, Status>>;
 
+    #[instrument(skip(self))]
     async fn download_file(
         &self,
         request: Request<DownloadFileRequest>,
     ) -> Result<Response<Self::DownloadFileStream>, Status> {
         unimplemented!()
     }
+
+    #[instrument(skip(self))]
     async fn upload_file(
         &self,
         request: Request<Streaming<UploadFileRequest>>,
     ) -> Result<Response<UploadFileResponse>, Status> {
         unimplemented!()
     }
+
+    #[instrument(skip(self))]
     async fn list_files(
         &self,
         _request: Request<ListFilesRequest>,
@@ -51,41 +57,44 @@ impl FileService for FileServiceImpl {
         let directory = Arc::clone(&self.directory);
         let tx_error = tx.clone();
 
-        tokio::spawn(async move {
-            let result = async move {
-                let mut dir_stream = fs::read_dir(directory.as_ref()).await?;
+        tokio::spawn(
+            async move {
+                let result = async move {
+                    let mut dir_stream = fs::read_dir(directory.as_ref()).await?;
 
-                while let Some(dir_entry) = dir_stream.next_entry().await? {
-                    let file_metadata = dir_entry.metadata().await?;
-                    if !file_metadata.is_file() {
-                        continue;
+                    while let Some(dir_entry) = dir_stream.next_entry().await? {
+                        let file_metadata = dir_entry.metadata().await?;
+                        if !file_metadata.is_file() {
+                            continue;
+                        }
+                        let file_name = dir_entry.file_name().into_string().map_err(|e| {
+                            anyhow!("OsString convertion failed: {:?}", e.to_string_lossy())
+                        })?;
+                        let file_size = file_metadata.len();
+                        tx.send(Ok(ListFilesResponse {
+                            name: file_name,
+                            size: file_size,
+                        }))
+                        .await?;
                     }
-                    let file_name = dir_entry.file_name().into_string().map_err(|e| {
-                        anyhow!("OsString convertion failed: {:?}", e.to_string_lossy())
-                    })?;
-                    let file_size = file_metadata.len();
-                    tx.send(Ok(ListFilesResponse {
-                        name: file_name,
-                        size: file_size,
-                    }))
-                    .await?;
+
+                    Ok::<(), anyhow::Error>(())
                 }
+                .await;
 
-                Ok::<(), anyhow::Error>(())
-            }
-            .await;
+                if let Err(err) = result {
+                    error!(%err);
+                    let send_result = tx_error
+                        .send(Err(Status::internal("Failed to list files")))
+                        .await;
 
-            if let Err(_) = result {
-                // TODO: log error
-                let send_result = tx_error
-                    .send(Err(Status::internal("Failed to list files")))
-                    .await;
-
-                if let Err(_) = send_result {
-                    // TODO: log error
+                    if let Err(err) = send_result {
+                        error!(%err);
+                    }
                 }
             }
-        });
+            .in_current_span(),
+        );
 
         Ok(Response::new(ReceiverStream::new(rx)))
     }
--- server/src/main.rs
+++ server/src/main.rs
@@ -4,7 +4,16 @@ use server::{cli::Cli, server_main};
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    tracing_subscriber::fmt()
+        .with_file(true)
+        .with_line_number(true)
+        .with_thread_ids(true)
+        .with_target(false)
+        .init();
+
     let args = Cli::try_parse()?;
+
     server_main(&args).await?;
+
     Ok(())
 }

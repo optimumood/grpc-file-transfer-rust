fec8df55cb35859e96d26e907874b38a5eaedcb5
optimumood <optimumood@gmail.com>
2023-01-15 03:23:50 +0100

    Added e2e test for empty list files

--- e2e_tests/src/tests.rs
+++ e2e_tests/src/tests.rs
@@ -7,9 +7,10 @@ mod e2e_test_context;
 
 #[test_context(E2ETestContext)]
 #[test]
-fn test_list_command_success(ctx: &mut E2ETestContext) {
+fn test_list_files_success(ctx: &mut E2ETestContext) {
     ctx.start_server();
     ctx.create_test_files("abc", "hello");
+    ctx.create_test_files("xyz", "grpc");
 
     let mut cmd = Command::cargo_bin("client").unwrap();
     let assert = cmd
@@ -21,5 +22,23 @@ fn test_list_command_success(ctx: &mut E2ETestContext) {
     assert
         .success()
         .stdout(predicate::str::contains("File name  Size"))
-        .stdout(predicate::str::contains("abc        5B"));
+        .stdout(predicate::str::contains("abc        5B"))
+        .stdout(predicate::str::contains("xyz        4B"));
+}
+
+#[test_context(E2ETestContext)]
+#[test]
+fn test_list_files_empty_success(ctx: &mut E2ETestContext) {
+    ctx.start_server();
+
+    let mut cmd = Command::cargo_bin("client").unwrap();
+    let assert = cmd
+        .args(["--port", &ctx.port.to_string()])
+        .args(["--address", "::1"])
+        .arg("list")
+        .assert();
+
+    assert
+        .success()
+        .stdout(predicate::str::ends_with("File name  Size").trim());
 }

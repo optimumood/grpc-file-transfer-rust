569f842c5ecb1d2acea98123e69c73639a078565
optimumood <optimumood@gmail.com>
2023-01-15 06:28:20 +0100

    Added waiting in e2e tests until server has started

--- e2e_tests/src/e2e_test_context.rs
+++ e2e_tests/src/e2e_test_context.rs
@@ -6,6 +6,8 @@ use std::process::{Child, Command};
 use std::sync::Once;
 use tempdir::TempDir;
 use test_context::TestContext;
+use tokio::runtime::Runtime;
+use tonic::transport::Channel;
 
 static BUILD: Once = Once::new();
 
@@ -93,15 +95,45 @@ impl E2ETestContext {
     const SERVER_BIN_NAME: &str = "server";
 
     pub fn start_server(&mut self) {
+        let server_ip_address = "::1";
         let server_bin_path = cargo_bin(Self::SERVER_BIN_NAME);
         let server_child = Command::new(server_bin_path)
             .args(["--port", &self.port.to_string()])
-            .args(["--address", "::1"])
+            .args(["--address", server_ip_address])
             .args(["--directory", self.server.dir.path().to_str().unwrap()])
             .spawn()
             .expect("server failed to start");
 
+        self.wait_for_server(server_ip_address);
+
         self.server.process = Some(server_child);
+    }
+
+    fn wait_for_server(&self, server_ip_address: &str) {
+        let rt = Runtime::new().unwrap();
+        let server_address = format!("http://[{}]:{}", server_ip_address, self.port);
+        let mut retries: u32 = 0;
+        const MAX_RETRIES: u32 = 10;
+        loop {
+            retries += 1;
+
+            if let Err(err) = rt.block_on(async {
+                Channel::builder(server_address.parse().unwrap())
+                    .connect()
+                    .await?;
+
+                Ok::<(), tonic::transport::Error>(())
+            }) {
+                eprintln!("Couldn't connect to server: {}", err);
+                std::thread::sleep(std::time::Duration::from_secs(1));
+            } else {
+                break;
+            }
+
+            if retries >= MAX_RETRIES {
+                panic!("Couldn't connect to server");
+            }
+        }
     }
 
     pub fn create_test_file(&mut self, app_type: AppType, file_name: &str, file_content: &str) {
